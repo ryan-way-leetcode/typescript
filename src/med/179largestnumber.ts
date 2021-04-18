00b6cdd7bf286c653e2620f2d3f1f1345db6f3e5
Ryan Way <ryan.keith.way@gmail.com>
2021-04-18 15:34:32 -0700

    179: comment

--- src/med/179largestnumber.ts
+++ src/med/179largestnumber.ts
@@ -1,3 +1,18 @@
+//https://leetcode.com/problems/largest-number/
+/* Given a list of non-negative intergers nums, arrage them such that they form
+ * the larget number.
+ *
+ * Returning a string due to maximum value of result for integer
+ *
+ * Approach:
+ *
+ * Sort numbers by  concatentated string value, reverse, then join
+ *
+ * 1) convert nums to strings
+ * 2) sort by string comparator
+ * 3) reverse, and return joined list
+ *
+ */
 export function largestNumber(nums: number[]): string {
   var sNums: string[] = nums.map((x) => x.toString());
   sNums.sort((x, y) => (x+y).localeCompare(y+x));
